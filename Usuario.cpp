e4d56ee319821d2724b23cb810553d4e1a6f9c60
Jose Antonio De la Mora <jadelamoraloyola@gmail.com>
2026-09-17 14:12:29 +0200

    Usuario

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "Usuario.h"
+#include "Libro.h"
+#include <iostream>
+
+Usuario::Usuario(std::string nombre, std::string apellido, std::string dni) {
+    this->nombre = nombre;
+    this->apellido = apellido;
+    this->dni = dni;
+    cantidadReservados=0;
+    cantidadHistorial=0;
+}
+
+void Usuario::reservarLibro(Libro libro) {
+    Usuario::librosReservados[cantidadReservados++] = libro;
+    Usuario::librosHistorial[cantidadHistorial++] = libro;
+}
+
+void Usuario::devolverLibro() {
+    cantidadReservados--;
+}
+
+void Usuario::printReservas() {
+    for (int i = 0; i < cantidadReservados; i++) {
+        std::cout << i << ": " << Usuario::librosReservados[i].getTitulo << std::endl; //Supongo que pondran getTitulo en Libro
+    }
+}
+
+void Usuario::printHistorial() {
+    for (int i = 0; i < cantidadHistorial; i++) {
+        std::cout<< i << ": " << Usuario::librosHistorial[i].getTitulo << std::endl; //Supongo que pondran getTitulo en Libro
+    }
+}
+
+bool Usuario::verificarLibro(Libro libro) {
+    int i=0;
+    bool verificar = false;
+    do {
+        if (libro.getTitulo() == Usuario::librosReservados[i].getTitulo()) {
+            verificar = true;
+        }
+        i++;
+    } while (verificar == false && i<cantidadReservados);
+    return verificar;
+}
--- Usuario.h
+++ Usuario.h
@@ -5,8 +5,25 @@
 #ifndef BIBLIOTECA_USUARIO_H
 #define BIBLIOTECA_USUARIO_H
 
+#include <string>
+#include "Libro.h"
 
 class Usuario {
+private:
+    std::string nombre;
+    std::string apellido;
+    std::string dni;
+    Libro librosReservados[50];
+    int cantidadReservados;
+    Libro librosHistorial[50];
+    int cantidadHistorial;
+public:
+    Usuario(std::string nombre, std::string apellido, std::string dni);
+    void reservarLibro(Libro libro);
+    void devolverLibro();
+    void printReservas();
+    void printHistorial();
+    bool verificarLibro(Libro libro);
 };
 
 
