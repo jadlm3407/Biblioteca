4b9530ca1d8b09c55e3abfc7509b087056a29cb8
ppeii7 <peihaosun2007@gmail.com>
2026-09-18 11:05:55 +0200

    Update Biblioteca.h
    Primeros métodos de Biblioteca.h pero sin desarrollar.

--- Biblioteca.h
+++ Biblioteca.h
@@ -4,10 +4,27 @@
 
 #ifndef BIBLIOTECA_BIBLIOTECA_H
 #define BIBLIOTECA_BIBLIOTECA_H
+#include "Usuario.h"
 
 
 class Biblioteca {
-    
+
+
+
+
+public:
+//    Comprobar qué libros hay disponibles
+//   Ver el historial de libros de una persona
+//    Ver si una persona tiene un libro o no
+//    Sacar un libro (asignárselo a una persona)
+//    Devolver un libro (desasignárselo a dicha persona)
+    Biblioteca();
+
+    void librosDisponibles();
+    bool tieneLibro(Usuario usuario);
+    void asignarLibro(Usuario usuario);
+    void designarLibro(Usuario usuario);
+
 };
 
 
